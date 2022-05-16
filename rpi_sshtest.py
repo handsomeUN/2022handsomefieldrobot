cabe1fb4e4953a44d3aeae58ba90b36d617bbb81
handsomeUN <88881231+handsomeUN@users.noreply.github.com>
2022-05-16 23:05:53 +0800

    Update rpi_sshtest.py

--- rpi_sshtest.py
+++ rpi_sshtest.py
@@ -2,7 +2,12 @@ import cv2
 ESC = 27
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 cap = cv2.VideoCapture(0)
-ratio = cap.get(cv2.CAP_PROP_FRAME_WIDTH)/cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+try:
+    ratio = cap.get(cv2.CAP_PROP_FRAME_WIDTH)/cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+except:
+    print("Exception!!")
+    
 WIDTH = 600
 HEIGHT = int(WIDTH/ratio)
 while True:
