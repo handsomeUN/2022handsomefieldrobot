b6f03e0ef0a515651a8be7f34d54bb9e230f749e
handsomeUN <88881231+handsomeUN@users.noreply.github.com>
2022-05-16 22:32:06 +0800

    Update rpi_sshtest.py

--- rpi_sshtest.py
+++ rpi_sshtest.py
@@ -2,11 +2,9 @@ import cv2
 ESC = 27
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
 cap = cv2.VideoCapture(0)
-#ratio = cap.get(cv2.CAP_PROP_FRAME_WIDTH)/cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-#WIDTH = 600
-#HEIGHT = int(WIDTH/ratio)
+ratio = cap.get(cv2.CAP_PROP_FRAME_WIDTH)/cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 WIDTH = 600
-HEIGHT = 400
+HEIGHT = int(WIDTH/ratio)
 while True:
     ret, frame = cap.read()
     frame = cv2.resize(frame, (WIDTH, HEIGHT))
